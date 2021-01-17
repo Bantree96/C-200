b71724d3064eab80bceee39e12e78e6586fd9f88
Bantree96 <51081373+Bantree96@users.noreply.github.com>
2021-01-17 15:24:36 +0900

    A007_ConsoleWriteLine

---
+++ A007_ConsoleWriteLine/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A007_ConsoleWriteLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool b = true;
+            char c = 'A';
+            decimal d = 1.234m;    // m은 decimal형의 접미사
+            double e = 1.23456789;
+            float f = 1.23456789f; // f는 float형의 접미사
+            int i = 1234;
+            string s = "Hello";
+
+            Console.WriteLine("bool : " + b);
+            Console.WriteLine("char : " + c);
+            Console.WriteLine("decimal : " + d);
+            Console.WriteLine("double : " + e);
+            Console.WriteLine("float : " + f);
+            Console.WriteLine("int : " + i);
+            Console.WriteLine("string : " + s);
+
+
+        }
+    }
+}
