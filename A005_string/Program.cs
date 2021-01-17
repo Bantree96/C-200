8f3ba98317b1f8014d65bca7580c60c7c69be1d0
Bantree96 <51081373+Bantree96@users.noreply.github.com>
2021-01-17 15:07:05 +0900

    A005_string

--- A005_string/Program.cs
+++ A005_string/Program.cs
@@ -10,6 +10,16 @@ namespace A005_string
     {
         static void Main(string[] args)
         {
+            string a = "hello";
+            string b = "h";
+
+            b = b + "ello";
+            Console.WriteLine(a == b);
+            Console.WriteLine("b = " + b);
+
+            int x = 10;
+            string c = b + '!' + " " + x;
+            Console.WriteLine("c = " + c);
         }
     }
 }
