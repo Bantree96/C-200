fba67e4df8e35603a7d2da01934734dc04e9452d
Bantree96 <51081373+Bantree96@users.noreply.github.com>
2021-01-19 09:33:49 +0900

    A018_DivideByZero 예외처리문

---
+++ A018_DivideByZero/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A018_DivideByZero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10, y = 0;
+            try 
+            {
+                Console.WriteLine(x / y);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message); // 예외를 설명하는 메세지를 가져온다.
+            }
+        }
+    }
+}
