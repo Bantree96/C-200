a016a558fe59622f75612b7db58160be5d500fed
Bantree96 <51081373+Bantree96@users.noreply.github.com>
2021-01-21 15:12:50 +0900

    A039_Object

---
+++ A039_Object/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A039_Object
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 123;
+            object o = i;   // i의 값을 박싱하여 o로 복사합니다.
+            i = i + 10;     // i의 값을 바꿉니다. o의 값은 변하지 않습니다.
+            int j = (int)o; // o의 값을 언박싱 하여 j로 복사합니다.
+
+            Console.WriteLine("The value-type value i = {0}", i);
+            Console.WriteLine("The object-type value o = {0}", o);
+            Console.WriteLine("The value-type value j = {0}", j);
+
+            object p = o;
+            o = 100;
+            Console.WriteLine("The object-type value o = {0}", o);
+            Console.WriteLine("The object-type value p = {0}", p);
+        }
+    }
+}
