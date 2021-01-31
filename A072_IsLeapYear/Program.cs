965fb0e5f90f6ff9e7a9c2ceb9ce257b75d00d8c
남지원 <51081373+Bantree96@users.noreply.github.com>
2021-01-31 16:07:32 +0900

    A072_IsLeapYear

---
+++ A072_IsLeapYear/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A072_IsLeapYear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  2001년~2100년 사이의 윤년을 찾는 프로그램
+            for(int i=2001; i<=2100; i++)
+                if (IsLeapYear(i))
+                    Console.Write("{0} ", i);
+            Console.WriteLine();
+        }
+
+        private static bool IsLeapYear(int year)
+        {
+            return year % 4 == 0 && (year % 100 != 0) || (year % 400 == 0);
+        }
+    }
+}
