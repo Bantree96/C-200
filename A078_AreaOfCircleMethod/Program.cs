c25850d2aa5bd08e7285c0fce07a15c90f023d0d
Bantree96 <51081373+Bantree96@users.noreply.github.com>
2021-02-05 11:26:00 +0900

    A078_AreaOfCircleMethod

---
+++ A078_AreaOfCircleMethod/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A078_AreaOfCircleMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (double r = 1; r <= 10; r++)
+                Console.WriteLine("원의 면적은 : {0,2} = {1,7:F2}", r, AreaOfCircle(r));
+        }
+        private static double AreaOfCircle(double r)
+        {
+            return Math.PI * r * r;
+        }
+    }
+}
