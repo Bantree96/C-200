94e2c8b1920063b19c13ccd30eb62384bbd47b42
Bantree96 <51081373+Bantree96@users.noreply.github.com>
2021-02-05 11:17:33 +0900

    A077_PowerMethod

---
+++ A077_PowerMethod/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A077_PowerMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 20; i++)
+                Console.WriteLine("2 ^ {0,2} = {1, 7}", i, Power(2, i));
+        }
+        private static int Power(int x, int y)
+        {
+            int p = 1;
+            for (int i = 1; i<= y; i++)
+                p *= x;
+            return p;
+        }
+    }
+}
