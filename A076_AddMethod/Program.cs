daec883ebc8909ce15cd871a304f600b50bbfc07
Bantree96 <51081373+Bantree96@users.noreply.github.com>
2021-02-05 11:10:16 +0900

    A076_AddMethod

---
+++ A076_AddMethod/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A076_AddMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1부터 100까지의 합 : {0}", Add(1, 100));
+            Console.WriteLine("101부터 200까지의 합 : {0}", Add(101, 200));
+        }
+        private static int Add(int x, int y)
+        {
+            int sum = 0;
+            for(int i=x; i<=y; i++)
+            {
+                sum += i;
+            }
+            return sum;
+        }
+    }
+}
