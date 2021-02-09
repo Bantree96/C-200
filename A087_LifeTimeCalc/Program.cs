0356734440e7ef6e1cfa48f2a07210f2a28a4426
남지원 <51081373+Bantree96@users.noreply.github.com>
2021-02-09 18:39:17 +0900

    A087_LifeTimeCalc

---
+++ A087_LifeTimeCalc/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A087_LifeTimeCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("생년월일 시분초를 입력하세요:");
+            DateTime date1 = DateTime.Parse(Console.ReadLine());
+            DateTime date2 = DateTime.Now;
+
+            TimeSpan interval = date2 - date1;
+            Console.WriteLine("탄생 시간: {0}", date1);
+            Console.WriteLine("현재 시간: {0}", date2);
+            Console.WriteLine("생존 시간: {0}", interval.ToString());
+            Console.WriteLine($"당신은 지금 이 순간까지 {interval.Days}일 {interval.Hours}시간" + $" {interval.Minutes}분 {interval.Seconds}초를 살았습니다.");
+
+            
+        }
+    }
+}
