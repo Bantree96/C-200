e149b0312b89cd62553bd8ca1987cc6a5c1e6128
남지원 <51081373+Bantree96@users.noreply.github.com>
2021-02-09 19:35:54 +0900

    A090_params

---
+++ A090_params/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A090_params
+{
+    class Program
+    {
+        public static void PrintIntParams(params int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+        }
+        
+        public static void PrintObjectParams(params object[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            PrintIntParams(1, 2, 3, 4);
+            PrintObjectParams(1, 1.234, 'a', "test");
+            PrintObjectParams();
+
+            int[] myIntArray = { 5, 6, 7, 8, 9 };
+            PrintIntParams(myIntArray);
+
+            object[] myObjArray = { 2, 2.345, 'b', "test", "again" };
+            PrintObjectParams(myObjArray);
+
+            PrintObjectParams(myIntArray); // 배열의 내용을 출력할 수는 없고 myIntArray자체를 object타입의 매개변수 하나로 간주해 출력
+        }
+    }
+}
