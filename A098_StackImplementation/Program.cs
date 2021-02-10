9fa7c842eb4bc9d33a7482ee6fbf6563d80f5de7
남지원 <51081373+Bantree96@users.noreply.github.com>
2021-02-10 19:37:36 +0900

    A098_StackImplementation

---
+++ A098_StackImplementation/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A098_StackImplementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mystack<int> stack = new Mystack<int>();
+            Random r = new Random();
+
+            for(int i =0; i< 10; i++)
+            {
+                int val = r.Next(100);
+                stack.Push(val);
+                Console.Write($"Push({val}) ");
+            }
+            Console.WriteLine();
+
+            for(int i=0; i<10; i++)
+            {
+                Console.Write($"Pop()={stack.Pop()},");
+            }
+        }
+    }
+}
---
+++ A098_StackImplementation/stack.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A098_StackImplementation
+{
+    class Mystack<T>
+    {
+        const int maxSize = 10;
+        private T[] arr = new T[maxSize];
+        private int top;
+
+        public Mystack() // 생성자
+        {
+            top = 0; 
+        }
+
+        public void Push(T val)
+        {
+            if(top < maxSize)
+            {
+                arr[top] = val;     // 배열에 값 삽입
+                ++top;              // 포인터 증가
+            }else
+            {
+                Console.WriteLine("Stack Full");    // 가득차면 출력
+                return;
+            }
+        }
+
+        public T Pop()
+        {
+            // 포인터가 0보다 크면
+            if(top > 0)
+            {
+                --top;              // 포인터 감소
+                return arr[top];    // 배열값 return
+            } else
+            {
+                Console.WriteLine("Stack Empty");
+                return default(T);
+
+            }
+        }
+    }
+}
