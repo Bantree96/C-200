ace0737e4a569fa2ece9c940a764a5a0dbdc2b21
남지원 <51081373+Bantree96@users.noreply.github.com>
2021-02-14 13:27:26 +0900

    A113_DelegateExample

---
+++ A113_DelegateExample/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A113_DelegateExample
+{
+    class Program
+    {
+        delegate bool MemberTest(int a);
+
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 3, 5, 4, 2, 6, 4, 6, 8, 54, 23, 4, 6, 4 };
+
+            Console.WriteLine("짝수의 개수:" + Count(arr, IsEven));
+            Console.WriteLine("홀수의 개수:" + Count(arr, IsOdd));
+        }
+
+        // count메소드는 배열과 델리게이트 메소드를 매개변수로 한다.
+        static int Count(int[] a, MemberTest testMethod)
+        {
+            int cnt = 0;
+            foreach(var n in a)
+            {
+                if (testMethod(n) == true)
+                    cnt++;
+            }
+            return cnt;
+        }
+
+        // 델리게이트 메소드
+        static public bool IsOdd(int n) { return n % 2 != 0; }
+        static public bool IsEven(int n) { return n % 2 != 1; }
+    }
+}
