a404d801f099866bf6a840a3167605ba31fa061d
남지원 <51081373+Bantree96@users.noreply.github.com>
2021-02-17 20:47:31 +0900

    A129_FormClass

---
+++ A129_FormClass/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace A129_FormClass
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.ClientSize = new Size(500, 500); // 객체가 생성될때 ClientSize 속성을 500 x 500으로 바꿉니다.
+
+            Form f2 = new Form2(); // Form2를 생성
+            this.AddOwnedForm(f2); // Form1에 Form2를 추가
+
+            f2.Show();
+        }
+    }
+}
